f2eaf18e63f9ddffd30e5170ede035a3cac1355d
piperck <w19890729wai@hotmail.com>
2016-09-23 23:22:25 +0800

    add_flask_hook

--- fuel/app.py
+++ fuel/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from fuel.views import medivac
 from myconf.online_config import OnlineConfig
+from fuel.init_db import db_session
 
 # init application
 app = Flask(__name__)
@@ -10,3 +11,9 @@ app.config.from_object(OnlineConfig)
 
 # add blue_print
 app.register_blueprint(medivac)
+
+
+# the request en gone away auto shutdown SQLAlchemy connect
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
