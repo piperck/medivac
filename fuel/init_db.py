1d055a70cab6109bb2940908c290f1b208abd96d
piperck <w19890729wai@hotmail.com>
2016-09-23 23:06:39 +0800

    fix

--- fuel/init_db.py
+++ fuel/init_db.py
@@ -15,5 +15,5 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import fuel.models
+    import fuel.models.user
     Base.metadata.create_all(bind=engine)
