e5920e1ddd26ebd13b7c0631db8512b55096be69
piperck <w19890729wai@hotmail.com>
2016-09-23 22:55:37 +0800

    change_config

--- fuel/myconf/dev_config.py
+++ fuel/myconf/dev_config.py
@@ -1,3 +1,2 @@
 class DevConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://medivac:shiya520@127.0.0.1:3306/medivac_terran?charset=utf8mb4"
--- fuel/myconf/online_config.py
+++ fuel/myconf/online_config.py
@@ -1,2 +1,2 @@
 class OnlineConfig(object):
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://medivac:shiya520@127.0.0.1:3306/medivac_terran?charset=utf8mb4"
+    pass
