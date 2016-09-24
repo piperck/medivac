27464e3cccfef9c02a8aa19493dc11d6d99afce1
piperck <w19890729wai@hotmail.com>
2016-09-24 18:33:35 +0800

    fix save file name messy display

--- fuel/app.py
+++ fuel/app.py
@@ -1,13 +1,9 @@
 from flask import Flask
 from fuel.views import medivac
-from myconf.online_config import OnlineConfig
 from fuel.init_db import db_session
 
 # init application
 app = Flask(__name__)
-
-# add_config
-app.config.from_object(OnlineConfig)
 
 # add blue_print
 app.register_blueprint(medivac)
