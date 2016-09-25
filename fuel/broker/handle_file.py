06daa0f344a956e8e959eb1dc9669ceb89f08b3e
piperck <w19890729wai@hotmail.com>
2016-09-25 14:45:37 +0800

    change_file_path

--- fuel/broker/handle_file.py
+++ fuel/broker/handle_file.py
@@ -14,20 +14,20 @@ def generate_uri_from_put(file_name, real_file):
     # add_database
     split = file_name.rsplit(".", 1)
     if len(split) == 1:
-        file_name, file_extension_name = split[0], ''
+        _, file_extension_name = split[0], ''
     else:
-        file_name, file_extension_name = split
+        _, file_extension_name = split
 
     # born unique no
-    x = str(uuid.uuid4())+file_name
-    file_hash = hashlib.md5(x).hexdigest()
+    x = str(uuid.uuid4())
+    file_hash = str(hashlib.md5(x).hexdigest())
 
     # save file
     file_uri = os.path.join(UPLOAD_FOLDER, file_hash)
     with io.open(file_uri, "w", encoding="utf-8") as f:
         f.write(real_file)
 
-    file_map = FileMap(file_name=file_hash, file_extension_name=file_extension_name, file_uri=file_uri)
+    file_map = FileMap(file_path=file_hash, file_extension_name=file_extension_name, file_uri=file_uri)
     db_session.add(file_map)
     db_session.commit()
 
