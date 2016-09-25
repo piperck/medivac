9e7a8af0f4b2bbb6af2b8387a19cad722419ee99
piperck <w19890729wai@hotmail.com>
2016-09-25 14:59:37 +0800

    ajust file name display

--- fuel/models/file_map.py
+++ fuel/models/file_map.py
@@ -7,14 +7,14 @@ class FileMap(Base):
     __tablename__ = 'file_map'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    file_path = Column(String(100), nullable=False)
+    file_name = Column(String(100), nullable=False)
     file_extension_name = Column(String(5), nullable=False)
     file_uri = Column(Text(), nullable=False)
     create_time = Column(DateTime, default=datetime.datetime.now())
     update_time = Column(DateTime, default=datetime.datetime.now())
 
-    def __init__(self, file_path, file_extension_name, file_uri):
-        self.file_path = file_path
+    def __init__(self, file_name, file_extension_name, file_uri):
+        self.file_name = file_name
         self.file_extension_name = file_extension_name
         self.file_uri = file_uri
 
