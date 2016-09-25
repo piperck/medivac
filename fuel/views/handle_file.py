38665d9ea43704c2f595e15b7d83a1c5e364992c
piperck <w19890729wai@hotmail.com>
2016-09-25 18:45:36 +0800

    file get view complete

--- fuel/views/handle_file.py
+++ fuel/views/handle_file.py
@@ -2,7 +2,7 @@
 from flask import request
 from fuel.const import MaxFile
 from fuel.views import medivac
-from fuel.broker.handle_file import generate_url
+from fuel.broker.handle_file import generate_url, get_file_by_short_url
 
 
 @medivac.route("/<file_name>", methods=["PUT"])
@@ -12,3 +12,13 @@ def handle_upload_file(file_name):
         return "Too big file, your file must less than %s byte for now :);" % MaxFile.MAX_CONTENT_LENGTH
 
     return generate_url(file_name, real_file)
+
+
+@medivac.route("/<short_url>", methods=["GET"])
+def get_file(short_url):
+    try:
+        short_url = str(short_url)
+    except (ValueError, TypeError):
+        return "What are you doing?....."
+
+    return get_file_by_short_url(short_url)
