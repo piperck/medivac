ba46c5f1175623dcce43fd13a57c32f605ce03b4
piperck <w19890729wai@hotmail.com>
2016-09-25 14:15:41 +0800

    file name hash handle

--- fuel/broker/handle_file.py
+++ fuel/broker/handle_file.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import io
 import os
+import uuid
+import hashlib
 from fuel.models.file_map import FileMap
 from fuel.init_db import db_session
 
@@ -9,18 +11,23 @@ UPLOAD_FOLDER = "/home/command_center"
 
 
 def generate_uri_from_put(file_name, real_file):
-    # save file
-    file_uri = os.path.join(UPLOAD_FOLDER, file_name)
-    with io.open(file_uri, "w", encoding="utf-8") as f:
-        f.write(real_file)
-
     # add_database
     split = file_name.rsplit(".", 1)
     if len(split) == 1:
         file_name, file_extension_name = split[0], ''
     else:
         file_name, file_extension_name = split
-    file_map = FileMap(file_name=file_name, file_extension_name=file_extension_name, file_uri=file_uri)
+
+    # born unique no
+    x = str(uuid.uuid4())+file_name
+    file_hash = hashlib.md5(x).hexdigest()
+
+    # save file
+    file_uri = os.path.join(UPLOAD_FOLDER, file_hash)
+    with io.open(file_uri, "w", encoding="utf-8") as f:
+        f.write(real_file)
+
+    file_map = FileMap(file_name=file_hash, file_extension_name=file_extension_name, file_uri=file_uri)
     db_session.add(file_map)
     db_session.commit()
 
